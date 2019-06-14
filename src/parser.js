69cdf6802fb28fbfd5db7eaa7c180877db45c7e5
Shelkoudenko Akim <ag_shelkoudenko@bingo-boom.com>
2019-06-14 15:18:28 +0300

    jsdoc parser & description in graph

--- src/parser.js
+++ src/parser.js
@@ -1,38 +1,94 @@
+import doctrine from "doctrine";
+
+function parseJsDoc({ source }) {
+  let match = source.toString().match(/\/\*\*([\s\S]*?)\*\//g);
+  let parsedData = {
+    name: "no name",
+    actions: [],
+    requests: []
+  };
+
+  if (source.toString().indexOf("empty point ${key}") === -1 && match !== null) {
+    const counters = { action: 0, request: 0 };
+    match.forEach(text => {
+      const ast = doctrine.parse(text, { unwrap: true });
+
+      const obj = ast.tags.reduce((acc, { description, title, name }) => {
+        if (name) {
+          parsedData.name = name;
+        }
+        if (title === "action" || title === "request") {
+          if (acc.type) {
+            parsedData[`${acc.type}s`].push(acc);
+            acc = {};
+          }
+          acc.id = counters[title]++;
+          acc.title = description;
+          acc.type = title;
+        } else if (title === "description") {
+          acc.description = description;
+        } else if (title === "format") {
+          acc.format = description;
+        }
+
+        return acc;
+      }, {});
+
+      if (obj.type) {
+        parsedData[`${obj.type}s`].push(obj);
+      }
+    });
+
+    return parsedData;
+  }
+
+  return { name: "no data", actions: [], requests: [] };
+}
+
 export default class Graph {
-    constructor() {
-        this.nodes = [];
-    }
+  constructor() {
+    this.nodes = [];
+  }
 
-    parseData([name, acid, ...children]) {
-        this.addNode([name, acid, ...children]);
-        children.forEach(child => this.parseData(child));
+  parseData(data) {
+    const [, , ...children] = data;
+    this.addNode(data);
+    children.forEach(child => this.parseData(child));
 
-        return this;
-    }
+    return this;
+  }
 
-    addNode([name, acid, ...children]) {
-        this.nodes.push({
-            name,
-            acid,
-            children: children.map(([name]) => name)
-        });
+  addNode(data) {
+    const [name, acid, ...children] = data;
+    const {
+      entities,
+      prop: { source }
+    } = data;
 
-        return this;
-    }
+    this.nodes.push({
+      name,
+      acid,
+      children: children.map(([name]) => name),
+      entities: entities.length,
+      source: parseJsDoc({ source })
+    });
 
-    getNode(nodeName) {
-        const find = this.nodes.find(({ name }) => name === nodeName);
+    return this;
+  }
 
-        return find ? find : null;
-    }
+  getNode(nodeName) {
+    const find = this.nodes.find(({ name }) => name === nodeName);
 
-    getChildren(nodeName) {
-        const find = this.nodes.find(({ name }) => name === nodeName);
+    return find ? find : null;
+  }
 
-        return find ? find.children : null;
-    }
+  getChildren(nodeName) {
+    const find = this.nodes.find(({ name }) => name === nodeName);
 
-    get nodesCount() {
-        return this.nodes.length;
-    }
+    return find ? find.children : null;
+  }
+
+  get nodesCount() {
+    return this.nodes.length;
+  }
 }
